599c6845b274c8646b8439d52680e85664d551a8
Sonrisa <sonrisa722611@gmail.com>
2018-01-25 15:48:27 -0800

    Update jest toEqual instead of toBe for object

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
-const collectDrops = () => {};
+function findMax(start, end, inputArr) {
+  let tempMaxIndex = -Infinity;
+
+  for (let i = start; i < end; i++) {
+    if (
+      inputArr[i] >= inputArr[tempMaxIndex] ||
+      inputArr[tempMaxIndex] === undefined
+    ) {
+      tempMaxIndex = i;
+    }
+  }
+  return tempMaxIndex;
+}
+
+const collectDrops = input => {
+  // Rule out invalid input
+  if (!Array.isArray(input)) return 'not valid input';
+  if (input.length <= 1) return [0, 0, 0];
+
+  let result = [];
+  for (let i = 0; i < input.length; i++) {
+    // find leftMax & right Max
+    let leftIndex = findMax(0, i, input);
+    let rightIndex = findMax(i + 1, input.length, input);
+    // find minWall
+    let leftVal = input[leftIndex];
+    let rightVal = input[rightIndex];
+
+    let minWall = Math.min(leftVal, rightVal);
+
+    // find a drop
+    if (input[i] < minWall) {
+      // if not between the same wall
+      if (result[0] !== leftIndex || result[1] !== rightIndex) {
+        result[0] = leftIndex;
+        result[1] = rightIndex;
+        result[2] = minWall - input[i];
+      } else {
+        // if does between the same wall
+        result[2] += minWall - input[i];
+      }
+    }
+  }
+
+  return result.length === 0 ? [0, 0, 0] : result;
+};
 module.exports = collectDrops;
--- index.test.js
+++ index.test.js
@@ -1,25 +1,29 @@
 const collectDrops = require('./index.js');
 
 test('shoud collectDrops return an array', () => {
-  expect(Array.isArray(collectDrops([3, 1, 1, 3]))).toBe(true);
+  expect(Array.isArray(collectDrops([3, 1, 1, 3]))).toEqual(true);
 });
 
 test('shoud collectDrops return [0,0,0] when no water [1,2,3]', () => {
-  expect(collectDrops([1, 2, 3])).toBe([0, 0, 0]);
+  expect(collectDrops([1, 2, 3])).toEqual([0, 0, 0]);
 });
 
 test('shoud collectDrops return [0,0,0] when no water [3,2,1]', () => {
-  expect(collectDrops([3, 2, 1])).toBe([0, 0, 0]);
+  expect(collectDrops([3, 2, 1])).toEqual([0, 0, 0]);
 });
 
 test('shoud collectDrops return [0,0,0] when no water [1,2,1]', () => {
-  expect(collectDrops([1, 2, 1])).toBe([0, 0, 0]);
+  expect(collectDrops([1, 2, 1])).toEqual([0, 0, 0]);
 });
 
 test('shoud collectDrops return [0,0,0] when no wall', () => {
-  expect(collectDrops([])).toBe([0, 0, 0]);
+  expect(collectDrops([])).toEqual([0, 0, 0]);
 });
 
 test('shoud collectDrops return coorrect amount of water', () => {
-  expect(collectDrops([3, 1, 3])).toBe([0, 2, 2]);
+  expect(collectDrops([3, 1, 3])).toEqual([0, 2, 2]);
+});
+
+test('shoud collectDrops return coorrect amount of water when walls are same height', () => {
+  expect(collectDrops([3, 2, 3, 1, 1, 1, 3])).toEqual([2, 6, 6]);
 });
